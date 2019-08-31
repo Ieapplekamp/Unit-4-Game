ada3b9e9b8173e1c9d875cf8fe6a0c63a2ade1b6
Ian Applekamp <ieapplekamp@gmail.com>
2019-08-30 23:46:37 -0400

    conditional statement alerts win or lose

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,79 +9,117 @@ $(document).ready(function () {
     var playerScore = 0;
     
     //test idea
-    var purple = Math.floor((Math.random() * (12 - 1)) + 1);
+    var purple = Math.floor((Math.random() * 12) + 1);
     console.log('this is purple' + purple);
     //var blue = Math.floor((Math.random() * (12 - 1)) + 1);
     //console.log('this is blue' + blue);
-    var red = Math.floor((Math.random() * (12 - 1)) + 1);
+    var red = Math.floor((Math.random() * (12)) + 1);
     console.log('this is red' + red);
-    var orange = Math.floor((Math.random() * (12 - 1)) + 1);
+    var orange = Math.floor((Math.random() * (12)) + 1);
     console.log('this is orange' + orange);
-    var green = Math.floor((Math.random() * (12 - 1)) + 1);
+    var green = Math.floor((Math.random() * (12)) + 1);
     console.log('this is green' + green);
-    var yellow = Math.floor((Math.random() * (12 - 1)) + 1);
+    var yellow = Math.floor((Math.random() * (12)) + 1);
     console.log('this is yellow' + yellow);
     
     
 
-    initialScore = Math.floor((Math.random() * (120 - 19) ) + 19 );
-
-    //initialScore = Math.floor(Math.random() * 120);
-
-    $("#target-number").append(initialScore);
-
-    $("#purple").on("click", function () {
-        
-        purple = parseInt(purple);
-        playerScore += purple;
+    initialScore = Math.floor((Math.random() * (120 - 19)) + 19);
     
-        $("#player-score").html(playerScore);
 
-    })
+    function start() {
+        //initialScore = Math.floor(Math.random() * 120);
 
-    $("#yellow").on("click", function () {
+        $("#target-number").append(initialScore);
+
+        $("#purple").on("click", function () {
         
-        yellow = parseInt(yellow)
-        playerScore += yellow;
-
-        $("#player-score").html(playerScore);
-
-    })
-
-    $("#red").on("click", function () {
-        
-        red = parseInt(red)
-        playerScore += red;
-
-        $("#player-score").html(playerScore);
-
-    })
-
-    $("#orange").on("click", function () {
-        
-        orange = parseInt(orange)
-        playerScore += orange;
-
-        $("#player-score").html(playerScore);
-
-    })
-
-    $("#green").on("click", function () {
-        
-        green = parseInt(green)
-        playerScore -= green;
-
-        $("#player-score").html(playerScore);
-
-    })
-
+            purple = parseInt(purple);
+            playerScore += purple;
     
-    //      Math.floor(Math.Random() * x)
+            $("#player-score").html(playerScore);
+            if (playerScore == initialScore) {
+                wins++;
+                alert('You Won!');
+            } else {
+                if (playerScore > initialScore) {
+                alert('You Lose!');
+                }
+            }
+        })
+
+        $("#yellow").on("click", function () {
+        
+            yellow = parseInt(yellow)
+            playerScore += yellow;
+
+            $("#player-score").html(playerScore);
+            if (playerScore == initialScore) {
+                wins++;
+                alert('You Won!');
+            } else {
+                if (playerScore > initialScore) {
+                alert('You Lose!');
+                }
+            }
+        })
+
+        $("#red").on("click", function () {
+        
+            red = parseInt(red)
+            playerScore += red;
+
+            $("#player-score").html(playerScore);
+            if (playerScore == initialScore) {
+                wins++;
+                alert('You Won!');
+            } else {
+                if (playerScore > initialScore) {
+                alert('You Lose!');
+                }
+            }
+        })
+
+        $("#orange").on("click", function () {
+        
+            orange = parseInt(orange)
+            playerScore += orange;
+
+            $("#player-score").html(playerScore);
+            if (playerScore == initialScore) {
+                wins++;
+                alert('You Won!');
+            } else {
+                if (playerScore > initialScore) {
+                alert('You Lose!');
+                }
+            }
+        })
+
+        $("#green").on("click", function () {
+        
+            green = parseInt(green)
+            playerScore -= green;
+
+            $("#player-score").html(playerScore);
+            if (playerScore == initialScore) {
+                wins++;
+                alert('You Won!');
+            } else {
+                if (playerScore > initialScore) {
+                alert('You Lose!');
+                }
+            }
+        })
+
+        
+        //      Math.floor(Math.Random() * x)
+    }
 
     // the randomly generated initialScore needs to be between 19 & 120
     // each crystal needs to hold a random value (probably between 1 and 12). When the crystal is clicked push/add that to the playerScore.
     // when the playerScore matches the initial score, player wins
     // if player score goes over initialScore, player loses
     // when the game is won or loss have a reset to generate a new initialScore and set the playerScore back to 0 WITHOUT changed the wins or losses 
-
+    start();
 });
