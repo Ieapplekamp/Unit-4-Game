2efbbefdf1fba2731cd1b5e911494d0d8dd10339
Ian Applekamp <ieapplekamp@gmail.com>
2019-08-30 21:25:04 -0400

    each stone/button generates a random number. added a function for each and pushed the new numbers into the html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,87 @@
 // what variables do I need?
 // don't forget a reset
 
-var wins = 0;
-var losses = 0;
-var initialScore = "";
-var playerScore = 0;
+$(document).ready(function () {
+    
+    var wins = 0;
+    var losses = 0;
+    var initialScore = "";
+    var playerScore = 0;
+    
+    //test idea
+    var purple = Math.floor((Math.random() * (12 - 1)) + 1);
+    console.log('this is purple' + purple);
+    //var blue = Math.floor((Math.random() * (12 - 1)) + 1);
+    //console.log('this is blue' + blue);
+    var red = Math.floor((Math.random() * (12 - 1)) + 1);
+    console.log('this is red' + red);
+    var orange = Math.floor((Math.random() * (12 - 1)) + 1);
+    console.log('this is orange' + orange);
+    var green = Math.floor((Math.random() * (12 - 1)) + 1);
+    console.log('this is green' + green);
+    var yellow = Math.floor((Math.random() * (12 - 1)) + 1);
+    console.log('this is yellow' + yellow);
+    
+    
 
-//      Math.floor(Math.Random() * x)
+    initialScore = Math.floor((Math.random() * (120 - 19) ) + 19 );
 
-// the randomly generated initialScore needs to be between 19 & 120
-// each crystal needs to hold a random value (probably between 1 and 12). When the crystal is clicked push/add that to the playerScore.
-// when the playerScore matches the initial score, player wins
-// if player score goes over initialScore, player loses
-// when the game is won or loss have a reset to generate a new initialScore and set the playerScore back to 0 WITHOUT changed the wins or losses 
+    //initialScore = Math.floor(Math.random() * 120);
 
+    $("#target-number").append(initialScore);
+
+    $("#purple").on("click", function () {
+        
+        purple = parseInt(purple);
+        playerScore += purple;
+    
+        $("#player-score").html(playerScore);
+
+    })
+
+    $("#yellow").on("click", function () {
+        
+        yellow = parseInt(yellow)
+        playerScore += yellow;
+
+        $("#player-score").html(playerScore);
+
+    })
+
+    $("#red").on("click", function () {
+        
+        red = parseInt(red)
+        playerScore += red;
+
+        $("#player-score").html(playerScore);
+
+    })
+
+    $("#orange").on("click", function () {
+        
+        orange = parseInt(orange)
+        playerScore += orange;
+
+        $("#player-score").html(playerScore);
+
+    })
+
+    $("#green").on("click", function () {
+        
+        green = parseInt(green)
+        playerScore -= green;
+
+        $("#player-score").html(playerScore);
+
+    })
+
+    
+    //      Math.floor(Math.Random() * x)
+
+    // the randomly generated initialScore needs to be between 19 & 120
+    // each crystal needs to hold a random value (probably between 1 and 12). When the crystal is clicked push/add that to the playerScore.
+    // when the playerScore matches the initial score, player wins
+    // if player score goes over initialScore, player loses
+    // when the game is won or loss have a reset to generate a new initialScore and set the playerScore back to 0 WITHOUT changed the wins or losses 
+
+});
