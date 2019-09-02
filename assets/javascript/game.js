5c0743ffac7065a5cd3c3dae5246f201bdb96a1f
Ian Applekamp <ieapplekamp@gmail.com>
2019-09-01 20:18:10 -0400

    fixed the issue with the getting things to reset. added a wins and losses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,171 +5,154 @@ $(document).ready(function () {
     var initialScore = "";
     var playerScore = 0;
     
-    //test idea
-    var purple = Math.floor((Math.random() * 12) + 1);
-    console.log('purple' + purple);
-    //var blue = Math.floor((Math.random() * (12 - 1)) + 1);
-    //console.log('this is blue' + blue);
-    var red = Math.floor((Math.random() * 12) + 1);
-    console.log('red' + red);
-    var orange = Math.floor((Math.random() * 12) + 1);
-    console.log('orange' + orange);
-    var green = Math.floor((Math.random() * 12) + 1);
-    console.log('green' + green);
-    var yellow = Math.floor((Math.random() * 12) + 1);
-    console.log('yellow' + yellow);
-    
-    
-
-    initialScore = Math.floor((Math.random() * (120 - 19)) + 19);
-
+    //test idea - 6/10 shit started to work 
+    // var purple = Math.floor((Math.random() * 12) + 1);
+    // // //var blue = Math.floor((Math.random() * (12 - 1)) + 1);
+    // // //console.log('this is blue' + blue);
+    // var red = Math.floor((Math.random() * 12) + 1);
+    // var orange = Math.floor((Math.random() * 12) + 1);
+    // var green = Math.floor((Math.random() * 12) + 1);
+    // var yellow = Math.floor((Math.random() * 12) + 1);
+    // // initialScore = Math.floor((Math.random() * (120 - 19)) + 19);
 
     function start() {
         
-        // var playerScore = 0;
-        // var purple = Math.floor((Math.random() * 12) + 1);
-        // console.log('this is purple' + purple);
+        purple = Math.floor((Math.random() * 12) + 1);
         // //var blue = Math.floor((Math.random() * (12 - 1)) + 1);
         // //console.log('this is blue' + blue);
-        // var red = Math.floor((Math.random() * 12) + 1);
-        // console.log('this is red' + red);
-        // var orange = Math.floor((Math.random() * 12) + 1);
-        // console.log('this is orange' + orange);
-        // var green = Math.floor((Math.random() * 12) + 1);
-        // console.log('this is green' + green);
-        // var yellow = Math.floor((Math.random() * 12) + 1);
-        // console.log('this is yellow' + yellow);
-    
+        red = Math.floor((Math.random() * 12) + 1);
+        orange = Math.floor((Math.random() * 12) + 1);
+        green = Math.floor((Math.random() * 12) + 1);
+        yellow = Math.floor((Math.random() * 12) + 1);
+        
     
 
-        //initialScore = Math.floor((Math.random() * (120 - 19)) + 19);
-
+        initialScore = Math.floor((Math.random() * (120 - 19)) + 19);
+       
         $("#target-number").html(initialScore);
 
         $("#purple").on("click", function () {
         
-            purple = parseInt(purple);
             playerScore += purple;
-    
+            
             $("#player-score").html(playerScore);
             if (playerScore === initialScore) {
                 wins++;
-                alert('You Won!');
+                $("#wins").html(wins);
+                alert("You Won!");
                 restart();
             } else {
                 if (playerScore > initialScore) {
                     losses++;
-                    alert('You Lose!');
+                    $("#losses").html(losses);
+                    alert("You Lost!");
                     restart();
                 }
             }
+
         })
 
         $("#yellow").on("click", function () {
         
-            yellow = parseInt(yellow)
             playerScore += yellow;
 
             $("#player-score").html(playerScore);
             if (playerScore === initialScore) {
                 wins++;
-                alert('You Won!');
+                $("#wins").html(wins);
+                alert("You Won!");
                 restart();
             } else {
                 if (playerScore > initialScore) {
                     losses++;
-                    alert('You Lose!');
+                    $("#losses").html(losses);
+                    alert("You Lost!");
                     restart();
                 }
             }
+
         })
 
         $("#red").on("click", function () {
         
-            red = parseInt(red)
             playerScore += red;
 
             $("#player-score").html(playerScore);
             if (playerScore === initialScore) {
                 wins++;
-                alert('You Won!');
+                $("#wins").html(wins);
+                alert("You Won!");
                 restart();
             } else {
                 if (playerScore > initialScore) {
                     losses++;
-                    alert('You Lose!');
+                    $("#losses").html(losses);
+                    alert("You Lost!");
                     restart();
                 }
             }
+
         })
 
         $("#orange").on("click", function () {
-        
-            orange = parseInt(orange)
+   
             playerScore += orange;
 
             $("#player-score").html(playerScore);
             if (playerScore === initialScore) {
                 wins++;
-                alert('You Won!');
+                $("#wins").html(wins);
+                alert("You Won!");
                 restart();
             } else {
                 if (playerScore > initialScore) {
                     losses++;
-                    alert('You Lose!');
+                    $("#losses").html(losses);
+                    alert("You Lost!");
                     restart();
                 }
             }
+
         })
 
         $("#green").on("click", function () {
-        
-            green = parseInt(green)
+    
             playerScore -= green;
 
             $("#player-score").html(playerScore);
             if (playerScore === initialScore) {
                 wins++;
-                alert('You Won!');
+                $("#wins").html(wins);
+                alert("You Won!");
                 restart();
             } else {
                 if (playerScore > initialScore) {
                     losses++;
-                    alert('You Lose!');
+                    $("#losses").html(losses);
+                    alert("You Lost!");
                     restart();
                 }
             }
-        })
 
-        
+        })
+ 
     }
 
     start();
 
     function restart() {
 
-        var playerScore = 0;
+        if (playerScore > 0) {
+            purple = Math.floor((Math.random() * 12) + 1);
+            red = Math.floor((Math.random() * 12) + 1);
+            orange = Math.floor((Math.random() * 12) + 1);
+            yellow = Math.floor((Math.random() * 12) + 1);
+            green = Math.floor((Math.random() * 12) + 1);
+            initialScore = Math.floor((Math.random() * (120 - 19)) + 19);
+            $("#target-number").html(initialScore);
+            return playerScore = 0; 
+        }
 
-        $("#target-number").html(initialScore);
-        $("#player-score").html(playerScore);
-
-        var purple = Math.floor((Math.random() * 12) + 1);
-        //console.log('this is purple' + purple);
-        //var blue = Math.floor((Math.random() * (12 - 1)) + 1);
-        //console.log('this is blue' + blue);
-        var red = Math.floor((Math.random() * 12) + 1);
-        //console.log('this is red' + red);
-        var orange = Math.floor((Math.random() * 12) + 1);
-        //console.log('this is orange' + orange);
-        var green = Math.floor((Math.random() * 12) + 1);
-        //console.log('this is green' + green);
-        var yellow = Math.floor((Math.random() * 12) + 1);
-        //console.log('this is yellow' + yellow);
-    
-    
-
-        initialScore = Math.floor((Math.random() * (120 - 19)) + 19);
-        
-        
     }
+
 });
