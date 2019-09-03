528e61e820a5e6863e60303b5dc55881eb4bb5b2
Ian Applekamp <ieapplekamp@gmail.com>
2019-09-03 01:48:15 -0400

    deleted most comments and added a bit of styling and the rules of the game in a dropdown

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -14,52 +14,40 @@ $(document).ready(function () {
         green = Math.floor((Math.random() * 12) + 1);
         blue = Math.floor((Math.random() * 12) + 1);
         
-    
-
-        initialScore = Math.floor((Math.random() * (120 - 19)) + 19);
+        initialScore = Math.floor((Math.random() * 102) + 19);
        
         $("#target-number").html(initialScore);
 
         $("#purple").on("click", function () {
-        
             playerScore += purple;
             $("#player-score").html(playerScore);
             condtionals();
-
         })
 
         $("#blue").on("click", function () {
-        
             playerScore += blue;
             $("#player-score").html(playerScore);
             condtionals();
-
         })
 
         $("#red").on("click", function () {
-        
             playerScore += red;
             $("#player-score").html(playerScore);
             condtionals();
-
         })
 
         $("#orange").on("click", function () {
-   
             playerScore += orange;
             $("#player-score").html(playerScore);
             condtionals();
-
         })
 
         $("#green").on("click", function () {
-    
             playerScore -= green;
             $("#player-score").html(playerScore);
             condtionals();
-
         })
-
+        
         $("#yellow").on("click", function () {
             if (yellow > 1) {
                 wins++;
@@ -82,6 +70,11 @@ $(document).ready(function () {
                 $("#wins").html(wins);
                 alert("You Won!");
                 restart();
+            } else if (playerScore < -13) {
+                losses++;
+                $("#losses").html(losses);
+                alert("You went too far back, don't ignore the laws of physics..");
+                restart();
             } else {
                 if (playerScore > initialScore) {
                     losses++;
@@ -98,18 +91,20 @@ $(document).ready(function () {
 
     function restart() {
 
-        blue = Math.floor((Math.random() * 2) + 1);
+        yellow = Math.floor((Math.random() * 2) + 1);
 
-        if (playerScore > 0) {
+        if (playerScore > 0 || playerScore < -13) {
+
             purple = Math.floor((Math.random() * 12) + 1);
             red = Math.floor((Math.random() * 12) + 1);
             orange = Math.floor((Math.random() * 12) + 1);
-            yellow = Math.floor((Math.random() * 12) + 1);
+            blue = Math.floor((Math.random() * 12) + 1);
             green = Math.floor((Math.random() * 12) + 1);
             initialScore = Math.floor((Math.random() * (120 - 19)) + 19);
             $("#target-number").html(initialScore);
             $("#player-score").html(0);
             return playerScore = 0;
+
         }
 
     }
